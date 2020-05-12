9ae6fe8f4f4a62136e52eed2afd1697c3c5e3cac
Brandon <bbstilson@fastmail.com>
2020-05-12 12:51:25 -0700

    remove custom toString

--- emr_cluster_manager/src/org/bbstilson/config/Config.scala
+++ emr_cluster_manager/src/org/bbstilson/config/Config.scala
@@ -10,21 +10,4 @@ case class Config(
   bootstrapAction: Option[BootstrapAction],
   logUri: String,
   tags: List[Tag]
-) {
-
-  override def toString(): String = {
-    s"""
-       |Config(
-       |  clusterName: $clusterName,
-       |  masterInstanceType: $masterInstanceType,
-       |  driverMemory: $driverMemory,
-       |  workerInstanceType: $workerInstanceType,
-       |  numWorkers: $numWorkers,
-       |  steps: $steps,
-       |  bootstrapAction: $bootstrapAction,
-       |  logUri: $logUri,
-       |  tags: $tags
-       |)
-    """.stripMargin
-  }
-}
+)
